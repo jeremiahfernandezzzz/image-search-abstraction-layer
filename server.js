05cde0240d32a9a34ad76b316f9038be784ed5a3
Glitch (bright-newsprint) <none>
2017-11-16 16:11:53 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -1,39 +1,24 @@
-// server.js
-// where your node app starts
+var GoogleSearch = require('google-search')
+var googleSearch = new GoogleSearch({
+	key: 'AIzaSyDE142vj4kzR_qekIiMwHYe-b4fOh6b_z0',
+	cx: '006846818615894256664%3Adqfvw9xckim'
+})
 
-// init project
-var express = require('express');
-var app = express();
+var express = require("express")
+var app = new express()
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+app.get("/", function(req,res){
+	googleSearch.build({
+		q:"",
+		start: "0",
+		fileType: "",
+		gl: "",
+		lr: "lang_en",
+		num: "",
+		siteSearch: "",
+	},
+	 function(error, response){
+		res.send(response)
+	})
+})
+app.listen(process.env.PORT)
