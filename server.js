beff3f0357c5b98c9f6e2041f48155efab01635b
Glitch (bright-newsprint) <none>
2017-11-19 14:00:48 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var app = new express()
 var imageSearch = require('node-google-image-search');
  
 var results = imageSearch('<search-term>', callback(results){
-              }, 0, 5);
+              
+            }, 0, 5);
 
 app.listen(process.env.PORT)
