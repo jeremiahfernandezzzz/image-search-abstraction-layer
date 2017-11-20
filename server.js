39e55bea2437d6adace4cd1f67f81f725ebbd42a
Glitch (bright-newsprint) <none>
2017-11-20 08:18:47 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -2,21 +2,31 @@
 var express = require("express")
 var app = new express()
 
-var imageSearch = require('node-google-image-search');
+const GoogleImages = require('google-images');
  
-app.get('/',function(req,res){
-  var searchResults = imageSearch('hi', function(results){
-      res.send(JSON.stringify(results))
-  }, 0, 5);
+const client = new GoogleImages('006846818615894256664%3Adqfvw9xckim', 'AIzaSyDE142vj4kzR_qekIiMwHYe-b4fOh6b_z0');
  
-  //function callback(results) 
-  /*
-  if (searchResults){
-    res.send(searchResults)
-  } else {
-    res.send('no refs')
-  }
-  */
-})
+app.get('/', function(req,res){
+  client.search('Steve Angello')
+      .then(images => {
+        res.send(images)
+        /*
+        [{
+            "url": "http://steveangello.com/boss.jpg",
+            "type": "image/jpeg",
+            "width": 1024,
+            "height": 768,
+            "size": 102451,
+            "thumbnail": {
+                "url": "http://steveangello.com/thumbnail.jpg",
+                "width": 512,
+                "height": 512
+            }
+        }]
+         */
+    }).catch(
+      res.send('aaa')
+    )
+  })
 
 app.listen(process.env.PORT)
