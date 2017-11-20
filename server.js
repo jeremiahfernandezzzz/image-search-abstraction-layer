8a66b9ea7b968eac17cde6a16f02fb50238e7019
Glitch (bright-newsprint) <none>
2017-11-20 01:30:18 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -6,9 +6,12 @@ var GoogleImages = require('google-images');
 var client = new GoogleImages('006846818615894256664%3Adqfvw9xckim','AIzaSyDE142vj4kzR_qekIiMwHYe-b4fOh6b_z0')
 
 app.get('/:query', function(req,res){
+  //var a
   client.search(req.params.query).then(images=>{
-    res.send(images)
+    //a = images
   })
+  
+  //res.send(a)
 })
 
 app.listen(process.env.PORT)
