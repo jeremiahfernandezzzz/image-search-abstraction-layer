5e23fc20e0585d7cb3a93175f25651fbfb829f33
Glitch (bright-newsprint) <none>
2017-11-20 09:12:46 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -4,15 +4,14 @@ var app = new express()
 
 //var Bing = require('node-bing-api')({ accKey: '0290a1597c0e43cabc3c77dd16826b25'});
 
-var util = require('util'),
-  Bing = require('node-bing-api')({ accKey: '9ed464182a074e2da088a43c1d698fea'}),
-  searchBing = util.promisify(Bing.web.bind(Bing));
-
-Bing.images("Ninja Turtles", {
-  count: 15,   // Number of results (max 50) 
-  offset: 3    // Skip first 3 result 
-  }, function(error, res, body){
-    console.log(body);
+const Imgur = require('imgur-node');
+const client = new Imgur.Client('jeremiahfernandezzzz');
+ 
+app.get('/', function(req,res){
+  client.album.get('', (err, res) => {
+    if (err) console.error(err);
+    console.log(res);
   });
+})
 
 app.listen(process.env.PORT)
