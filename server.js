e0b72128a30c6272bd24192a99f790fcb85c1826
Glitch (bright-newsprint) <none>
2017-11-20 01:41:19 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var client = new GoogleImages('006846818615894256664%3Adqfvw9xckim','AIzaSyDE142
 
 app.get('/:query', function(req,res){
   //var a
-  client.search(req.params.query).then(images=>{
+  client.search('hello').then(images=>{
     //a = images
   })
   
