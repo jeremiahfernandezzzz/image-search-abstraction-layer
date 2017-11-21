0712015afda90a3074098e396ad2bc08fb1cfb0c
Glitch (bright-newsprint) <none>
2017-11-21 18:53:56 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ var app = new express()
 
 const GoogleImages = require('./myapi.js');
 
-const client = new GoogleImages('006846818615894256664:oilhtmgbk80', 'AIzaSyDE142vj4kzR_qekIiMwHYe-b4fOh6b_z0');
+const client = new GoogleImages('006846818615894256664:oilhtmgbk80', 'AIzaSyD2vIjre3uws0BGurMivbesspOn58DjorY');
 
 app.get('/', function (req,res){
 client.search('Steve Angello')
 	.then(images => {
-    console.log(images)
+    res.send(images)
 	}).catch(error =>{
     console.log(error)
   })
